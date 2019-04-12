521d214d262b276a1c34088f558a55cb93d2a99d
Jadson Araujo <jadson013@gmail.com>
2019-04-12 17:29:39 -0300

    AJustes

--- ControleDeAcesso_BaixoNivel.ino
+++ ControleDeAcesso_BaixoNivel.ino
@@ -9,32 +9,35 @@ char dataSource[3][8];
 
 volatile uint8_t ledVerde = 61;
 volatile uint8_t ledVermelho = 61;
-volatile uint8_t z = 0;
 
 ISR(TIMER0_OVF_vect) {
   if (ledVerde < 61) {
-    PORTD |= 0b00000001;
-    x++;
+    PORTD |= 0b00010000;
+    ledVerde++;
   } else
-    PORTD &= 0b11111110;
+    PORTD &= 0b11101111;
   if (ledVermelho < 61) {
-    PORTD |= 0b00000010;
-    x++;
+    PORTD |= 0b00000100;
+    ledVermelho++;
   } else
-    PORTD &= 0b11111101;
+    PORTD &= 0b11111011;
 }
 
 
 int main(void) {  
+  
   char BYTE_RECEBIDO[8];
+  uint16_t valorAD = 0;
+  uint8_t z = 0;
 
   DDRB &= 0b11111110;  //Pino PB0 como entrada (push-button)
   PORTB |= 0b00000001; //Pino PB0 com pull-up
 
   DDRB |= 0b00101100;  //Confugurando os pinos MOSI, SCK e SS como saída
   DDRB &= 0b11101111;  //Confugurando o pino MISO com entrada
+  PORTB &= 0b11111011;// colocando o SS pra 0
 
-  DDRD |= 0b00000111;  //Pinos PD0, PD1 e PD2 como saída (leds)
+  DDRD |= 0b00011100;  //Pinos PD0, PD1 e PD2 como saída (leds)
 
   //bit SPE habilita o SPI
   //bit MSTR = 1 habilita modo mestre
@@ -48,8 +51,8 @@ int main(void) {
   TCCR1A = _BV(COM1A1) | _BV(WGM10) | _BV(WGM11);
   TCCR1B = _BV(CS11) | _BV(WGM12);
 
-  ADMUX   |= 0b01000000; //AVCC with external capacitor at AREF pin
-  ADCSRA  |= 0b10000111; //Habilitando o ADC e seta o prescale para 128
+  ADMUX   |= _BV(REFS0); //AVCC with external capacitor at AREF pin
+  ADCSRA  |= _BV(ADEN) | 0b00000111; //Habilitando o ADC(ADEN) e seta o prescale para 128(três ultimos bits)
 
   //CONTAR TEMPO COM O CONTADOR ZERO
   TCCR0A &= 0b00000000;
@@ -58,22 +61,29 @@ int main(void) {
   sei();                          //Habilita a interrupção
 
   while (1) {
+
     z=0;
-    while (z < 8) {
+    while (z < 9) {
       //Transmite dados para o escravo
       //Nível PC0 vai para baixo
-      PORTB &= 0b11111011;
-      SPDR = BYTE_ENVIADO;
+      if(z<8) SPDR = BYTE_ENVIADO;
+      else SPDR = 0b00000000;
+
       while (!(SPSR & (0b10000000)));
-        BYTE_RECEBIDO[z++] = SPDR;
+
+      if(z>0)BYTE_RECEBIDO[z-1] = SPDR;
+      z++;
+      Serial.print(BYTE_RECEBIDO[z]);
     }
+
     if(ehCadastrado(BYTE_RECEBIDO))acessoLiberado();
     else acessoNegado();
+    Serial.print(PINB);
+    if(!(PINB & 0b00000001)) acessoLiberado();
 
-    ADCSRA |= 0b01000000;
-    while (!(ADCSRA & 0b00010000)) {
-      OCR1A = ADC;
-    }
+    // ADCSRA |= _BV(ADSC);
+    // while (!(ADCSRA & 0b00010000));
+    // valorAD = ADC;
   }
 
 }
