a93726a72ff1ad72129d22f9d8c3ca031ef4a03f
Jadson Araujo <jadson013@gmail.com>
2019-04-14 13:14:10 -0300

    Ajustes

--- ControleDeAcesso_BaixoNivel.ino
+++ ControleDeAcesso_BaixoNivel.ino
@@ -1,11 +1,11 @@
 #include <avr/io.h>
-#include <math.h>
 #include <avr/interrupt.h>
 #include <inttypes.h>
+#include <arduino.h>
 
 #define BYTE_ENVIADO 0b10000000
-
-char dataSource[3][8];
+#define QTD_BYTES  4
+unsigned char dataSource[3][QTD_BYTES];
 
 volatile uint8_t ledVerde = 61;
 volatile uint8_t ledVermelho = 61;
@@ -25,11 +25,11 @@ ISR(TIMER0_OVF_vect) {
 
 
 
-bool ehCadastrado(char *tag){
-  return false;
+bool ehCadastrado(unsigned char *tag){
+  //return false;
   bool ehIgual = false;
   for(int i=0; i<3; i++){
-    for(int j=0; j<8; j++){
+    for(int j=0; j<QTD_BYTES; j++){
       if(tag[j] != dataSource[i][j]){
         ehIgual = false;
         break;  
@@ -45,9 +45,9 @@ void acessoLiberado() {
   ledVerde = 0;
 }
 
-bool ehNulo(char* tag){
-  for(int i=0;i<8;i++){
-    if(tag[i]!=0) return false;
+bool ehNulo(unsigned char* tag){
+  for(int i=0;i<QTD_BYTES;i++){
+    if(tag[i]!=(unsigned char)128&&tag[i]!=(unsigned char)0) return false;
   }
   return true;  
 }
@@ -57,8 +57,8 @@ void acessoNegado() {
 }
 
 int main(void) {  
-  
-  char BYTE_RECEBIDO[8];
+  Serial.begin(9600);
+  unsigned char BYTE_RECEBIDO[QTD_BYTES];
   uint16_t valorAD = 0;
   uint8_t z = 0;
 
@@ -69,12 +69,12 @@ int main(void) {
   DDRB &= 0b11101111;  //Confugurando o pino MISO com entrada
   PORTB &= 0b11111011;// colocando o SS pra 0
 
-  DDRD |= 0b00011100;  //Pinos PD0, PD1 e PD2 como saída (leds)
+  DDRD |= 0b00111100;  //Pinos PD2, PD3, PD4 e PD5(reset) como saída (leds)
 
   //bit SPE habilita o SPI
   //bit MSTR = 1 habilita modo mestre
   //bits SPI2X, SPR1 e SPR0 configuram o clock
-  SPCR |= 0b01010000;
+  SPCR |= 0b01110000;
   SPSR |= 0b00000001;
 
   //Valores iniciais dos sinais PWM
@@ -93,19 +93,22 @@ int main(void) {
   sei();                          //Habilita a interrupção
 
   while (1) {
-
+    PORTD &= 0b11011111;
     z=0;
-    while (z < 9) {
+    while (z < QTD_BYTES+1) {
       //Transmite dados para o escravo
       //Nível PC0 vai para baixo
-      if(z<8) SPDR = BYTE_ENVIADO;
+      if(z<QTD_BYTES) SPDR = BYTE_ENVIADO;
       else SPDR = 0b00000000;
 
       while (!(SPSR & (0b10000000)));
-
+      
+      Serial.print("\n");
+      Serial.print(SPDR,BIN);
       if(z>0)BYTE_RECEBIDO[z-1] = SPDR;
       z++;
     }
+    PORTD |= 0b00100000;
     if(!ehNulo(BYTE_RECEBIDO)){
       if(ehCadastrado(BYTE_RECEBIDO))acessoLiberado();
       else acessoNegado();
